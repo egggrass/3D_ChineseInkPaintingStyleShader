81a74a4670e689bfab311093580a99372b127f76
egggrass <constancemiaoy@gmail.com>
2026-02-21 11:40:07 +0100

    placementscene

---
+++ Assets/Script/PlacementSystem.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlacementSystem : MonoBehaviour
+{
+    [SerializeField] 
+    GameObject mouseIndicator,cellIndicator;
+    [SerializeField]
+    private InputManager inputManager;
+    [SerializeField]
+    private Grid grid;
+
+    private void Update()
+    {
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
+        mouseIndicator.transform.position = mousePosition;
+
+       //cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+
+
+        cellIndicator.transform.position = grid.GetCellCenterWorld(gridPosition);
+    }
+
+}
