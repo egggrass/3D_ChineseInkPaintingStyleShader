edaded6daa22eb826a926c88325e259c24d2ddfd
egggrass <constancemiaoy@gmail.com>
2026-02-25 23:42:50 +0100

    ViewChange

---
+++ Assets/Script/DisplaySwitcher.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class DisplaySwitcher : MonoBehaviour
+{
+    public Camera camera1;
+    public Camera camera2;
+
+    private bool swapped = false;
+
+    public void Swap()
+    {
+        if (Display.displays.Length < 2)
+            return;
+
+        swapped = !swapped;
+
+        if (swapped)
+        {
+            camera1.targetDisplay = 1; // 去 Display2
+            camera2.targetDisplay = 0; // 去 Display1
+        }
+        else
+        {
+            camera1.targetDisplay = 0;
+            camera2.targetDisplay = 1;
+        }
+    }
+}
