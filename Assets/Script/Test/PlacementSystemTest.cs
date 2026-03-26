371b6030ae3ead8eda8bd07f01022012649926f6
egggrass <constancemiaoy@gmail.com>
2026-03-27 00:50:08 +0100

    grid placemenet test

---
+++ Assets/Script/Test/PlacementSystemTest.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlacementSystemTest : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mouseIndicator, cellIndicator;
+    [SerializeField]
+    private InputManager inputManager;
+    [SerializeField]
+    private Grid grid;
+    private void Update()
+    {
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridposition = grid.WorldToCell(mousePosition);
+        mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.CellToWorld(gridposition);
+    }
+}
